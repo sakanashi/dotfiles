75f3826895ef7d56f87c6694b79778059380cdaa
sakanashi <ryuutaroh.sakanashi@nts.ricoh.co.jp>
2017-07-21 10:40:58 +0900

    add-to-list jenkinsfile groovymode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -66,3 +66,4 @@
 (add-to-list 'auto-mode-alist '("\\.dig$" . yaml-mode))
 (add-hook 'yaml-mode-hook
           '(lambda () (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
+(add-to-list 'auto-mode-alist '("Jenkinsfile$" . groovy-mode))
