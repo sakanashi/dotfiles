41887c86745c697b8359e2041b65ae42ca64eb63
sakanashi <ryuutaroh.sakanashi@nts.ricoh.co.jp>
2017-07-25 09:22:50 +0900

    fix error

--- bin/disableJpaEventHandler.sh
+++ bin/disableJpaEventHandler.sh
@@ -2,9 +2,9 @@
 # https://stackoverflow.com/questions/19649847/eclipse-kepler-jpa-project-change-event-handler-waiting/28049565#28049565
 cd ~/.p2/pool/ # macはたぶんここ
 
-mkdir disabled
-mkdir disabled\features
-mkdir disabled\plugins
+mkdir -p disabled
+mkdir -p disabled/features
+mkdir -p disabled/plugins
 
 mv plugins/org.eclipse.jpt.* disabled/plugins
 mv features/org.eclipse.jpt.* disabled/features
