62d39cfbabcf896c75fba171d4ee2c758cd29461
sakanashi <ryuutaroh.sakanashi@nts.ricoh.co.jp>
2017-12-25 10:02:36 +0900

    fix error

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -69,9 +69,9 @@
 ;; ruby-block
 ;; endにカーソルを合わせると、そのendに対応する行をハイライトする
 ;; --------------------------------------------------
-(require 'ruby-block)
-(ruby-block-mode t)
-(setq ruby-block-highlight-toggle t)
+;(require 'ruby-block)
+;(ruby-block-mode t)
+;(setq ruby-block-highlight-toggle t)
 
 ;; ==================================================
 ;; language
