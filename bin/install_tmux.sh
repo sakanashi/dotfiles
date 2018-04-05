795026fecdda7d068ad260e99f0eb3a8db75f3a3
sakanashi <ryuutaroh.sakanashi@nts.ricoh.co.jp>
2018-04-05 10:04:58 +0900

    update

--- bin/install_tmux.sh
+++ bin/install_tmux.sh
@@ -24,10 +24,17 @@ make install
 wget https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
 tar zxf tmux-2.6.tar.gz
 cd tmux-2.6
+wget https://gist.githubusercontent.com/z80oolong/e65baf0d590f62fab8f4f7c358cbcc34/raw/3a3269767c863f3b25dbdf3849ed53150b28306e/tmux-HEAD-fb02df66-fix.diff
+patch -p1 < ./tmux-HEAD-fb02df66-fix.diff
 PKG_CONFIG_PATH=${HOME}/local/lib/pkgconfig ./configure --prefix=${HOME}/local
 make
 make install
 
+echo 'tmux install succeeded.'
+
+echo 'Get tpm'
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo 'Succeeded.'
 
 # curl -kLO https://github.com/tmux/tmux/releases/download/2.6/tmux-2.6.tar.gz
 # tar -zxvf tmux-2.6.tar.gz
