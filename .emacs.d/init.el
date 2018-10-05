254728da80f65ab9c8f6898e6dda2ed861e4d87b
sakanashi <ryuutaroh.sakanashi@nts.ricoh.co.jp>
2018-10-05 17:30:10 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -165,6 +165,10 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (setq helm-M-x-fuzzy-match t)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+;; For find-file etc.
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+;; For helm-find-files etc.
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 
 (require 'ag)
 (require 'helm-config)
